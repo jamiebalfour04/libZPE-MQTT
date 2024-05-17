707783babf4a81ca296f4d2ba4a6eff065cd9f2b
Jamie Balfour <jamie@balfour.scot>
2024-05-17 18:57:49 +0100

    Changed the functions and objects to their appropriate Get names.

--- src/Plugin.java
+++ src/Plugin.java
@@ -8,12 +8,12 @@ import java.util.Map;
 public class Plugin implements ZPELibrary {
 
   @Override
-  public ZPECustomFunction[] functions() {
+  public ZPECustomFunction[] GetFunctions() {
     return new ZPECustomFunction[0];
   }
 
   @Override
-  public Map<String, Class<?>> objects() {
+  public Map<String, Class<?>> GetObjects() {
     Map<String, Class<?>> m = new HashMap<>();
     m.put("mqtt_object", MQTTObject.class);
 
